13885149782701adcc984646b4ce3a635fcfa21e
Nayak <rahul.nyk@gmail.com>
2023-11-08 10:40:49 +0530

    adding old notebooks to the repo

--- helpers/prompts.py
+++ helpers/prompts.py
@@ -40,17 +40,17 @@ def graphPrompt(input: str, metadata={}, model="mistral-openorca:latest"):
     # print( chalk.blue(model_info))
 
     SYS_PROMPT = (
-        "You are a network graph maker who extracts terms and their relations from a given context"
+        "You are a network graph maker who extracts terms and their relations from a given context. "
         "You are provided with a context chunk (delimited by ```) Your task is to extract the ontology "
         "of terms mentioned in the given context. These terms should represent the key concepts as per the context. \n"
-        "Thought 1: While travelling through every sentence, Think about the key terms mentioned in it.\n"
+        "Thought 1: While traversing through each sentence, Think about the key terms mentioned in it.\n"
             "\tTerms may include object, entity, location, organization, person, \n"
             "\tcondition, acronym, documents, service, concept, etc.\n"
             "\tTerms should be as atomistic as possible\n\n"
-        "Thought 3: Think about how these terms can have one on one relation with other terms.\n"
-            "\tTerms that are mentioned in same sentence or the same paragraph are typically related to each other.\n"
+        "Thought 2: Think about how these terms can have one on one relation with other terms.\n"
+            "\tTerms that are mentioned in the same sentence or the same paragraph are typically related to each other.\n"
             "\tTerms can be related to many other terms\n\n"
-        "Thought 4: Find out the relation between each such related pair of terms. \n\n"
+        "Thought 3: Find out the relation between each such related pair of terms. \n\n"
         "Format your output as a list of json. Each element of the list contains a pair of terms"
         "and the relation between them, like the follwing: \n"
         "[\n"
